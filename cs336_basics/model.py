7489b7ba1b15a7747233e677f4248f419d9fc92a
johnfan12 <1637504800@qq.com>
2026-07-01 19:52:10 +0800

    implement multihead_self_attention

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -56,3 +56,36 @@ def rmsnorm(d_model: int, eps: float, weights: Tensor, in_features: Tensor):
 def scaled_dot_product_attention(Q: Tensor, K: Tensor, V: Tensor, mask: Tensor) -> Tensor:
     dp = ((Q @ K.transpose(-1, -2)) / math.sqrt(Q.shape[-1])).masked_fill(~mask, -torch.inf)  #[q k]
     return softmax(dp, dim=-1) @ V # [q, k] @ [k v] -> [q v]
+
+def multihead_self_attention(d_model: int, num_heads: int, 
+                             q_proj_weight: Tensor,
+                             k_proj_weight: Tensor,
+                             v_proj_weight: Tensor,
+                             o_proj_weight: Tensor,
+                             in_features: Tensor,
+                             )->Tensor:
+    assert d_model % num_heads == 0
+
+    q = F.linear(in_features, q_proj_weight) # [... sequence_length d_model] @ [d_model d_model] -> [... sequence_length d_model]
+    k = F.linear(in_features, k_proj_weight) # [... sequence_length d_model] @ [d_model d_model] -> [... sequence_length d_model]
+    v = F.linear(in_features, v_proj_weight) # [... sequence_length d_model] @ [d_model d_model] -> [... sequence_length d_model]
+
+    *batch_dims, sequence_length, input_dim = in_features.shape
+    assert input_dim == d_model
+    head_dim = d_model // num_heads
+
+    q = q.reshape(*batch_dims, sequence_length, num_heads, head_dim).transpose(-3, -2) # [... sequence_length d_model] -> [... sequence_length num_heads head_dim] -> [... num_heads sequence_length head_dim] 
+    k = k.reshape(*batch_dims, sequence_length, num_heads, head_dim).transpose(-3, -2) # [... sequence_length d_model] -> [... sequence_length num_heads head_dim] -> [... num_heads sequence_length head_dim] 
+    v = v.reshape(*batch_dims, sequence_length, num_heads, head_dim).transpose(-3, -2) # [... sequence_length d_model] -> [... sequence_length num_heads head_dim] -> [... num_heads sequence_length head_dim] 
+    
+    #scores = q @ k.transpose(-2, -1) # [... num_heads sequence_length head_dim] @ [... num_heads head_dim sequence_length] -> [... num_heads sequence_length sequence_length]
+    #scores = scores / math.sqrt(head_dim)
+    #atten = softmax(scores, dim=-1) # [... num_heads sequence_length sequence_length]
+     
+    causal_mask = torch.tril(torch.ones(sequence_length, sequence_length, dtype=torch.bool))
+    atten = scaled_dot_product_attention(q, k, v, causal_mask) # [... num_heads sequence_length sequence_length] @ [... num_heads sequence_length head_dim] -> [... num_heads sequence_length head_dim]
+
+    atten = atten.transpose(-3, -2) # [... num_heads sequence_length head_dim] -> [... sequence_length num_heads head_dim]
+    atten = atten.reshape(*batch_dims, sequence_length, d_model) # [... sequence_length num_heads head_dim] -> [... sequence_length d_model]
+
+    return F.linear(atten, o_proj_weight) # [... sequence_length d_model] @ [d_model d_model] -> [... sequence_length d_model]
